817bf989ad9dec51bb53b1b691c3cc6d7916ecb4
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-09-03 08:00:28 -0400

    add .env and dotenv and react-router-dom

--- src/component/CreateFact.js
+++ src/component/CreateFact.js
@@ -1,42 +1,41 @@
-// import React from 'react'
-// import { useNavigate } from "react-router-dom"
-// import {CreateFact}
-
-// function CreateFact() {
-//   return (
-//     <div>
-//       <h2>Create Fact!</h2>
-//     </div>
-//   )
-// }
-
-// export default CreateFact;
-
-import { useNavigate } from "react-router-dom"
+import React from 'react'
 
 
-function Create() {
-    const nav = useNavigate()
-
-    const createTheTodo = (e) => {
-        const todo = {description: e.target.description.value, complete: false}
-        createTodo(todo)
-        nav('/')
-    }
-
-return(
+function CreateFact() {
+  return (
     <div>
-        <h3>Create a Todo</h3>
-        <form onSubmit={createTheTodo}>
-            <input 
-            type='text' 
-            name='description' 
-            id='dsc'/> 
-            <br />
-            <input type='submit'/>
-        </form>
+      <h2>Create Fact!</h2>
     </div>
-)
+  )
 }
 
-export default Create
+export default CreateFact;
+
+// import { useNavigate } from "react-router-dom"
+
+
+// function Create() {
+//     const nav = useNavigate()
+
+//     const createTheTodo = (e) => {
+//         const todo = {description: e.target.description.value, complete: false}
+//         createTodo(todo)
+//         nav('/')
+//     }
+
+// return(
+//     <div>
+//         <h3>Create a Todo</h3>
+//         <form onSubmit={createTheTodo}>
+//             <input 
+//             type='text' 
+//             name='description' 
+//             id='dsc'/> 
+//             <br />
+//             <input type='submit'/>
+//         </form>
+//     </div>
+// )
+// }
+
+// export default Create
