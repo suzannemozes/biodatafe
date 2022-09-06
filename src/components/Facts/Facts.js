f1e64e703bf768f4eefd9fd55df84506b5f8e400
Suzanne Mozes <suzanne.mozes@gmail.com>
2022-09-06 13:44:55 -0400

    added 'no facts found' to facts.js

--- src/components/Facts/Facts.js
+++ src/components/Facts/Facts.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import FactItem from './FactItem';
-import Card from '../UI/Card';
-import FactsFilter from './FactsFilter';
-import './Facts.css';
+import FactItem from "./FactItem";
+import Card from "../UI/Card";
+import FactsFilter from "./FactsFilter";
+import "./Facts.css";
 
 const Facts = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2022');
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredFacts = props.items.filter(fact => {
+  const filteredFacts = props.items.filter((fact) => {
     return fact.date.getFullYear().toString() === filteredYear;
   });
 
   return (
     <div>
-      <Card className='facts'>
+      <Card className="facts">
         <FactsFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredFacts.map((fact) => (
-          <FactItem
-            key={fact.id}
-            title={fact.title}
-            description={fact.description}
-            date={fact.date}
-          />
-        ))}
+        {filteredFacts.length === 0 ? (
+          <p>No facts found.</p>
+        ) : (
+          filteredFacts.map((fact) => (
+            <FactItem
+              key={fact.id}
+              title={fact.title}
+              description={fact.description}
+              date={fact.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
